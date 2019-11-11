2968b0cf9395a63fb32eab19f16ee06453e904f7
anup-my <anup.my001@gmail.com>
2019-11-11 09:56:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-context-menu-yb6bfk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-context-menu-yb6bfk)
---
+++ app/context-menu-example.ts
@@ -0,0 +1,41 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material';
+
+@Component({
+  selector: 'context-menu-example',
+  templateUrl: 'context-menu-example.html'
+})
+export class ContextMenuExample {
+
+  items = [
+    {id: 1, name: 'Item 1'},
+    {id: 2, name: 'Item 2'},
+    {id: 3, name: 'Item 3'}
+  ];
+
+  @ViewChild(MatMenuTrigger)
+  contextMenu: MatMenuTrigger;
+
+  contextMenuPosition = { x: '0px', y: '0px' };
+
+  onContextMenu(event: MouseEvent, item: Item) {
+    event.preventDefault();
+    this.contextMenuPosition.x = event.clientX + 'px';
+    this.contextMenuPosition.y = event.clientY + 'px';
+    this.contextMenu.menuData = { 'item': item };
+    this.contextMenu.openMenu();
+  }
+
+  onContextMenuAction1(item: Item) {
+    alert(`Click on Action 1 for ${item.name}`);
+  }
+
+  onContextMenuAction2(item: Item) {
+    alert(`Click on Action 2 for ${item.name}`);
+  }
+}
+
+export interface Item {
+  id: number;
+  name: string;
+}
